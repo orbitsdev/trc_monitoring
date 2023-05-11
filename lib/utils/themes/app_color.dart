3d82715e4ded1a0b1bd7c36e311f2b7f07799ea3
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-05-11 19:01:52 +0800

    colors

---
+++ lib/utils/themes/app_color.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+class AppColor {
+
+  static const Color primary = Color(0xff2569FD) ;
+  static const Color secondary = Color(0xffE7F1FF) ;
+  static const Color green = Color(0xff1EB2A6) ;
+  static const Color orange = Color(0xffFF724C) ;
+  static const Color indigo = Color(0xff534BC7) ;
+  static const Color yellow = Color(0xffF4C300) ;
+  static const Color darkblue = Color(0xff14294A);
+  static const Color lightBlue = Color(0xff3879F0);
+  static const Color lightGreen = Color(0xff00CC96);
+  static const Color grey = Color(0xffDDDDDD) ;
+  static const Color greyFont = Color(0xff96A5BB) ;
+
+
+
+
+
+}
